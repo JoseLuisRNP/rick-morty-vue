79503b96b2443308496359c0320b80e531dfa719
José Luis Recio <reciojoselu@gmail.com>
2020-03-03 10:54:46 +0100

    Header

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bg-teal-800 flex justify-center">
+    <img :src="require('@/assets/rick-morty.png')" alt class="w-2/5" />
+  </div>
+</template>
+<script>
+export default {};
+</script>
